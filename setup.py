c2606be3c15bae320e2319bd1f860f96b38f6ac4
Koichi Yasuoka <yasuoka@kanji.zinbun.kyoto-u.ac.jp>
2019-11-27 16:33:48 +0900

    version 0.6.0

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ class qkanaInstall(install):
 
 setuptools.setup(
   name="udkundoku",
-  version="0.5.9",
+  version="0.6.0",
   description="Classical Chinese to Modern Japanese Translator",
   long_description=long_description,
   long_description_content_type="text/markdown",
