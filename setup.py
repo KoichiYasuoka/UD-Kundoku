7f5dc76e51539b6dc5efb398ccdba96eb5bfe366
Koichi Yasuoka <yasuoka@kanji.zinbun.kyoto-u.ac.jp>
2020-10-18 17:45:10 +0900

    version 1.4.7

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ class qkanaInstall(install):
 
 setuptools.setup(
   name="udkundoku",
-  version="1.4.6",
+  version="1.4.7",
   description="Classical Chinese to Modern Japanese Translator",
   long_description=long_description,
   long_description_content_type="text/markdown",
@@ -37,7 +37,7 @@ setuptools.setup(
   license="MIT",
   keywords="udkanbun nlp",
   packages=setuptools.find_packages(),
-  install_requires=["udkanbun>=2.4.0","unidic2ud>=2.4.9"],
+  install_requires=["udkanbun>=2.4.1","unidic2ud>=2.5.0"],
   python_requires=">=3.6",
   cmdclass={"install":qkanaInstall},
   package_data={
