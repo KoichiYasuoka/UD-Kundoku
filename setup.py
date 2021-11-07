1b84299e791e617d095ea16add6e86a82ab8476c
Koichi Yasuoka <yasuoka@kanji.zinbun.kyoto-u.ac.jp>
2021-11-07 13:54:27 +0900

    version 2.1.8

--- setup.py
+++ setup.py
@@ -2,32 +2,14 @@
 # coding=utf-8
 
 import setuptools
-from setuptools.command.install import install
 
 with open("README.md","r",encoding="UTF-8") as r:
   long_description=r.read()
 URL="https://github.com/KoichiYasuoka/UD-Kundoku"
-QKANA_URL="https://unidic.ninjal.ac.jp/unidic_archive/qkana/1603/UniDic-qkana_1603.zip"
-
-class qkanaInstall(install):
-  def run(self):
-    try:
-      import unidic2ud
-      if unidic2ud.dictlist().find("qkana\n")<0:
-        import subprocess
-        subprocess.check_call(["unidic2ud","--download=qkana"])
-    except:
-      import os,ssl,urllib.request,zipfile,glob
-      ssl._create_default_https_context=ssl._create_unverified_context
-      f,h=urllib.request.urlretrieve(QKANA_URL)
-      with zipfile.ZipFile(f) as z:
-        z.extractall("build")
-      os.renames(glob.glob("build/UniDic-qkana*")[0],"build/lib/udkundoku/qkana")
-    install.run(self)
 
 setuptools.setup(
   name="udkundoku",
-  version="2.1.7",
+  version="2.1.8",
   description="Classical Chinese to Modern Japanese Translator",
   long_description=long_description,
   long_description_content_type="text/markdown",
@@ -39,7 +21,6 @@ setuptools.setup(
   packages=setuptools.find_packages(),
   install_requires=["udkanbun>=3.3.0","unidic2ud>=2.8.8"],
   python_requires=">=3.6",
-  cmdclass={"install":qkanaInstall},
   package_data={
     "udkundoku":["./server/*.html","./server/*.js"],
   },
